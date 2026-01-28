c5f2f462c0d8db175ed5ff3145b1d8bb7df777e6
sohandsome0996 <sohandsome0996@gmail.com>
2026-01-29 00:32:47 +0800

    Update bot token and target channel ID

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from telegram.ext import (
 import time
 
 # -------------------------- 必改3项 --------------------------
-BOT_TOKEN = "你的投稿机器人Token"  # @BotFather获取
+BOT_TOKEN = "8527067919:AAG8PzWMX7mEJDXpZODxnjdO4TGZlFgdcNk"  # @BotFather获取
 ADMIN_ID = 7976084446             # 你的TG ID（@getidsbot获取）
-TARGET_CHANNEL_ID = "-100xxxxxxx" # 投稿目标频道ID（@getidsbot获取）
+TARGET_CHANNEL_ID = "-1003647769908" # 投稿目标频道ID（@getidsbot获取）
 # ----------------------------------------------------------------
 
 # 启动命令
@@ -80,3 +80,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
